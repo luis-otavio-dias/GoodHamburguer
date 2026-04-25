d4e67ee35b28572b1f12db5cf38ed161fcb1228d
luis-otavio-dias <luis777dias231@gmail.com>
2026-04-25 13:50:28 -0300

    Adiciona migrações iniciais

---
+++ GoodHamburguer/Migrations/20260425163315_InitialCreate.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GoodHamburguer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sanduiches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 10, nullable: false),
+                    Preco = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sanduiches", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SanduicheId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Total = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pedidos_Sanduiches_SanduicheId",
+                        column: x => x.SanduicheId,
+                        principalTable: "Sanduiches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Acompanhamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    Preco = table.Column<decimal>(type: "TEXT", nullable: false),
+                    PedidoId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Acompanhamentos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Acompanhamentos_Pedidos_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "Pedidos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Acompanhamentos",
+                columns: new[] { "Id", "Nome", "PedidoId", "Preco" },
+                values: new object[,]
+                {
+                    { 1, "Batata Frita", null, 2.00m },
+                    { 2, "Refrigerante", null, 2.50m }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Sanduiches",
+                columns: new[] { "Id", "Nome", "Preco" },
+                values: new object[,]
+                {
+                    { 1, "X Burguer", 5.00m },
+                    { 2, "X Egg", 4.50m },
+                    { 3, "X Bacon", 7.00m }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Acompanhamentos_PedidoId",
+                table: "Acompanhamentos",
+                column: "PedidoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedidos_SanduicheId",
+                table: "Pedidos",
+                column: "SanduicheId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Acompanhamentos");
+
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+
+            migrationBuilder.DropTable(
+                name: "Sanduiches");
+        }
+    }
+}
