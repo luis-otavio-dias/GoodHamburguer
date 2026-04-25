4f3d9f8aadbf2a7a8a23873007de014eabf901bc
luis-otavio-dias <luis777dias231@gmail.com>
2026-04-25 13:49:59 -0300

    Adiciona configuração de modelagem e dados iniciais para Sanduiches e Acompanhamentos

--- GoodHamburguer/Data/AppDbContext.cs
+++ GoodHamburguer/Data/AppDbContext.cs
@@ -12,4 +12,33 @@ public class AppDbContext : DbContext
     public DbSet<Sanduiche> Sanduiches { get; set; } = null!;
     public DbSet<Acompanhamento> Acompanhamentos { get; set; } = null!;
     public DbSet<Pedido> Pedidos { get; set; } = null!;
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Sanduiche>(s => 
+        {
+            s.HasKey(x => x.Id);
+            s.Property(x => x.Nome).IsRequired().HasMaxLength(10);
+            s.Property(x => x.Preco).IsRequired();
+
+            s.HasData(
+                new Sanduiche { Id = 1, Nome = "X Burguer", Preco = 5.00m },
+                new Sanduiche { Id = 2, Nome = "X Egg", Preco = 4.50m },
+                new Sanduiche { Id = 3, Nome = "X Bacon", Preco = 7.00m }
+            );
+        });
+
+        modelBuilder.Entity<Acompanhamento>(a =>
+        {
+            a.HasKey(x => x.Id);
+            a.Property(x => x.Nome).IsRequired().HasMaxLength(20);
+            a.Property(x => x.Preco).IsRequired();
+
+            a.HasData(
+                new Acompanhamento { Id = 1, Nome = "Batata Frita", Preco = 2.00m },
+                new Acompanhamento { Id = 2, Nome = "Refrigerante", Preco = 2.50m }
+            );
+        });
+    }
 }
