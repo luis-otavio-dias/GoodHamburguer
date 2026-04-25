626027abbdc18e76a49d4fbc72ec1448547495ce
luis-otavio-dias <luis777dias231@gmail.com>
2026-04-25 10:16:15 -0300

    Adiciona configuração inicial da aplicação e endpoints

---
+++ GoodHamburguer/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using GoodHamburguer.Data;
+using GoodHamburguer.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Add Entity Framework services
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=app.db"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPedidoEndpoints();
+
+app.Run();
